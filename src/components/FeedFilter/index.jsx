c6bd14ec983aeb3d38e37a2ae4949662dccca39f
LilDrikks <87108295+LilDrikks@users.noreply.github.com>
2023-01-20 12:36:07 -0300

    Conseguiiii scroll do carambaaa

--- src/components/FeedFilter/index.jsx
+++ src/components/FeedFilter/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {useState} from "react";
 
 function FeedFilter() {
   const listFilter = [
@@ -24,35 +24,33 @@ function FeedFilter() {
     "Assistidos",
     "Novidades para você",
   ];
-  const feedfilter = useRef();
   const [scroll, setScroll] = useState(0)
 
-  
-  useEffect(() => {
     function handleScroll(e) {
-      if(e.deltaY > 0){
-        setScroll(scroll -100)
-        console.log(scroll);
-        feedfilter.current.style.transform = `translateX(${scroll}px)`
-      }else{
-        setScroll(scroll +100)
-        console.log(scroll);
-        feedfilter.current.style.transform = `translateX(${scroll}px)`
-      }
+        if(e.deltaY > 0){
+          if(scroll < -1700) return
+          setScroll(scroll -100)
+          e.currentTarget.style.transform = `translateX(${scroll}px)`
+        }else{
+          if(scroll > -100) return
+          setScroll(scroll +100)
+          e.currentTarget.style.transform = `translateX(${scroll}px)`
+        }
     }
-    feedfilter.current.addEventListener('wheel', handleScroll)
-  }, [scroll])
+
 
   return (
     <div className="overflow-hidden">
       <ul
-        className={`flex p-3 pl-4 h-16 w-full feedfilter ease-out`}
-        ref={feedfilter}
+        className="flex p-3 pl-4 h-16 w-full duration-150"
+        onWheel={handleScroll}
+        style={{transform: `translateX(${scroll}px)`}}
       >
         {listFilter.map((item, index) => (
           <li
             key={index}
             className="text-[#f1f1f1] float-left text-[14px] pl-3"
+            
           >
             <p className="bg-[#272727] h-8 p-3 whitespace-nowrap flex items-center rounded-lg font-medium">
               {item}
