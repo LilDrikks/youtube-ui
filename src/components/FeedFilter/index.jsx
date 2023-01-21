dda365f899744a3d333b8ee48f9059f8935daa82
LilDrikks <87108295+LilDrikks@users.noreply.github.com>
2023-01-20 22:14:33 -0300

    agr sim scroll com responsividade da ul feed

--- src/components/FeedFilter/index.jsx
+++ src/components/FeedFilter/index.jsx
@@ -1,56 +1,56 @@
-import React, {useState} from "react";
+import React, { useState } from 'react'
 
 function FeedFilter() {
   const listFilter = [
-    "Tudo",
-    "Programação de computador...",
-    "Música",
-    "Jogos",
-    "Mixes",
-    "Treinamento com peso",
-    "Ao vivo",
-    "Freestyle Rap",
-    "League of Legends",
-    "Podcast",
-    "Álbuns",
-    "Inteligência artificial",
-    "Psicologia",
-    "Pensamentos",
-    "Música Lo-fi",
-    "Música brasileira",
-    "R&B",
-    "Vocal Music",
-    "Enviados recentemente",
-    "Assistidos",
-    "Novidades para você",
-  ];
+    'Tudo',
+    'Programação de computador...',
+    'Música',
+    'Jogos',
+    'Mixes',
+    'Treinamento com peso',
+    'Ao vivo',
+    'Freestyle Rap',
+    'League of Legends',
+    'Podcast',
+    'Álbuns',
+    'Inteligência artificial',
+    'Psicologia',
+    'Pensamentos',
+    'Música Lo-fi',
+    'Música brasileira',
+    'R&B',
+    'Vocal Music',
+    'Enviados recentemente',
+    'Assistidos',
+    'Novidades para você',
+  ]
   const [scroll, setScroll] = useState(0)
 
-    function handleScroll(e) {
-      const element = e.currentTarget
-        console.log(element.clientWidth)
+  function handleScroll(e) {
+    const element = e.currentTarget
 
-        if(e.deltaY > 0){
-          if(scroll < -1400) return
-          setScroll(scroll -100)
-        }else{
-          if(scroll > -100) return
-          setScroll(scroll +100)
-        }
+    if (e.deltaY > 0) {
+
+      
+      if (scroll < (element.clientWidth - 2500 - 40)) return
+      setScroll(scroll - 100)
+    } else {
+      if (scroll > -100) return
+      setScroll(scroll + 100)
     }
+  }
 
   return (
-    <div className="overflow-hidden">
+    <div className="overflow-hidden w-full">
       <ul
-        className="flex p-3 pl-4 h-16 w-[1500px] duration-150"
+        className="flex p-3 pl-4 h-16 w-full duration-150"
         onWheel={handleScroll}
-        style={{transform: `translateX(${scroll}px)`}}
+        style={{ transform: `translateX(${scroll}px)` }}
       >
         {listFilter.map((item, index) => (
           <li
             key={index}
             className="text-[#f1f1f1] float-left text-[14px] pl-3"
-            
           >
             <p className="bg-[#272727] h-8 p-3 whitespace-nowrap flex items-center rounded-lg font-medium">
               {item}
@@ -59,7 +59,7 @@ function FeedFilter() {
         ))}
       </ul>
     </div>
-  );
+  )
 }
 
-export default FeedFilter;
+export default FeedFilter
